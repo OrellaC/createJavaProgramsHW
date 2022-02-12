6b6ac7137ebaf0259cf4356a314fc1f2f51a2455
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-11 20:20:18 -0500

    "Sample Commit"

--- javaProgramHW/programHomework.java
+++ javaProgramHW/programHomework.java
@@ -59,8 +59,20 @@ public class programHomework {
 		System.out.println(colors);
 		
 		
-//		 Write a Java program to test a hash set is empty or not.
-		//
+//		Write a Java program to test a hash set is empty or not.
+		HashSet<String> h_set = new HashSet();
+		// use add() method to add values in the hash set
+		h_set.add("Red");
+		h_set.add("Green");
+		h_set.add("Black");
+		h_set.add("White");
+		h_set.add("Pink");
+		h_set.add("Yellow");
+		System.out.println("Original Hash Set: " + h_set);
+		System.out.println("Checking the above array list is empty or not! "+h_set.isEmpty());
+		System.out.println("Remove all the elements from a Hash Set: ");
+		h_set.removeAll(h_set);
+		System.out.println("Hash Set after removing all the elements "+h_set);
 //		Write a Java program to get the number of elements in a hash set
 		//
 //		Write a Java program to iterate through all elements in a hash list.
